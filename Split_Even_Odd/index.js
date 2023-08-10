1e9629d766a5aa9eed37bd5a80f8f3dea5334313
stephanecaron <stephane.caron@groupesavoie.com>
2023-08-10 14:28:35 -0300

    test

--- Split_Even_Odd/index.js
+++ Split_Even_Odd/index.js
@@ -1,8 +1,13 @@
-const numbers=[1,2,3,4,5,6,7,8,9,10];
+const numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,28,389,28293,293];
 const evenNumbers=[];
 const oddNumbers=[];
 const evenOdd=(number)=>{
-
+  if(number%2===0){
+    evenNumbers.push(number);
+  }else{
+    oddNumbers.push(number);
+  }
+}
 
 console.log('The numbers are ' + numbers);
 for(let i=0;i<numbers.length;i++){
