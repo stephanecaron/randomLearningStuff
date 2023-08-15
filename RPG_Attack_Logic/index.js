b475ea4e1a404f28ebf7b909b859cc0c24ec4685
stephanecaron <stephane.caron@groupesavoie.com>
2023-08-15 14:55:08 -0300

    Added input prompt for stats, now needs to be run via node index.js, glammed it up a bit

--- RPG_Attack_Logic/index.js
+++ RPG_Attack_Logic/index.js
@@ -1,7 +1,11 @@
+
 // exercise create a hit/miss/damage roll calculator for a DND'esc MELEE attack
 // step 2 is add an advantage/disadvantage roll
 // step 3 add ranged/spell attacks
-
+const prompt = require("prompt-sync")({ sigint: true });
+const redTextColor = '\x1b[31m';
+const greenTextColor = '\x1b[32m';
+const resetTextColor = '\x1b[0m';
 
 const weaponDamageMap = new Map([
     ['Club', '1d4'],
@@ -36,20 +40,19 @@ const weaponDamageMap = new Map([
     ['Crossbow, Heavy', '1d10'],
     ['Longbow', '1d8'],
   ]);
-
-const ac = 18;
-const abilityScore = 16;
-const weaponType = "Greataxe";
-const proficiencyLevel = 3;
-const damageModifier = 0;
-const advantage = 1; // 0 = normal, 1 = advantage, 2 = disadvantage
+const lines = ">------------------------------------------------------------------------------------<"
+// things that need to be input
+console.log(lines)
+var ac = Number(prompt("What is your target's AC?: "));
+let abilityScore = Number(prompt("What is your Ability Score? (Str or Dex depending on weapon type): "));
+let proficiencyLevel = Number(prompt("What is your proficiency in this weapon?: "));
+let advantage = Number(prompt("Do you have advantage(1), disadvantage(2), or neither(0)?: ")); // 0 = normal, 1 = advantage, 2 = disadvantage
+console.log(lines)
 // autocalculates
-const statModifier = (abilityScore-10)/2;
-const proficiencyBonus = 1+ Math.floor((proficiencyLevel-1)/4)
-const numberOfHits = (weaponDamageMap.get(weaponType))[0]
-const weaponDamage = weaponDamageMap.get(weaponType).split('d')[1]
+let statModifier = Math.floor((abilityScore-10)/2);
+let proficiencyBonus = 1 + Math.floor((proficiencyLevel-1)/4)
 
-console.log('Attacking target with a ' + weaponType + ' (' + weaponDamageMap.get(weaponType) + ')')
+//console.log('Attacking target with a ' + weaponType + ' (' + weaponDamageMap.get(weaponType) + ')')
 
 function diceRoll(max) {
     return Math.floor(Math.random()*max)+1
@@ -64,36 +67,51 @@ function advantageCalc(advantage, max) {
 function hitCalc(ac, statModifier, proficiencyBonus, advantage) {
     let roll = advantageCalc(advantage,20)
     if (roll === 1) {
-        console.log('Critical Miss :(')
+        console.log(redTextColor+'                                Critical Miss :('+resetTextColor)
+        console.log(lines)
         return "Miss"
     }
     if (roll === 20) {
-        console.log('Critical Hit!!!!!')
+        console.log(greenTextColor+ '                             Critical Hit!!!!!'+resetTextColor)
+        console.log(lines)
         return "Critical Hit"
     }
-    console.log('Your hit Accuracy is ' + (roll+statModifier+proficiencyBonus) + ' [('+roll+')Roll + ('+statModifier+')Ability Modifier + ('+proficiencyBonus+')Proficiency Bonus]')
-    console.log('Your targets AC is ' + ac)
-    if ((roll+statModifier+proficiencyBonus)>=ac) return "Hit"
-    console.log('You missed!')    
+    console.log('   Your hit Accuracy is ' + (roll+statModifier+proficiencyBonus) + ' [('+roll+')Roll + ('+statModifier+')Ability Modifier + ('+proficiencyBonus+')Proficiency Bonus]')
+    if ((roll+statModifier+proficiencyBonus)>=ac) {
+        console.log(greenTextColor+'           You have successfully hit your target, time to roll for damage'+resetTextColor)
+        console.log(lines)
+        return "Hit"
+    }
+    console.log(redTextColor + '                                    You missed!' + resetTextColor)
+    console.log(lines)    
     return "Miss"
+}
+
+
+function attack(ac, statModifier, proficiencyBonus, advantage) {
+    let hitType = hitCalc(ac, statModifier, proficiencyBonus, advantage)
+    if (hitType === "Miss") return 0
+    let weaponType = prompt("What is your weapon type? (Typos will cause this to crash bozo): ");
+    let damageModifier = Number(prompt("Are there flat damage modifiers, whether positive or negative? (enter the total): "));
+    console.log(lines)
+    let numberOfHits = (weaponDamageMap.get(weaponType))[0]
+    let weaponDamage = weaponDamageMap.get(weaponType).split('d')[1]
+    if (hitType === "Hit") return damageCalc(numberOfHits, weaponDamage, statModifier, damageModifier)
+    if (hitType === "Critical Hit") return (damageCalc(numberOfHits, weaponDamage, statModifier, damageModifier)+damageCalc(numberOfHits, weaponDamage, statModifier, damageModifier))
 }
 
 function damageCalc(numberOfHits, weaponDamage, statModifier, damageModifier) {
     let instanceDamage=0;
     for (i=0;i<numberOfHits;i++) {
-    let damage = diceRoll(weaponDamage)+statModifier+damageModifier
-    console.log('Damage('+[i+1]+'): ' + damage + ' [(' + (damage-statModifier-damageModifier) + ')Roll + ('+ statModifier + ')Ability Modifier + (' + damageModifier+ ')Damage Modifier]')
-    instanceDamage += damage
-}
-    console.log('Total damage for this instance : ' + instanceDamage)    
+        let damage = diceRoll(weaponDamage)+statModifier+damageModifier
+        console.log('          Damage('+[i+1]+'): ' + damage + ' [(' + (damage-statModifier-damageModifier) + ')Roll + ('+ statModifier + ')Ability Modifier + (' + damageModifier+ ')Damage Modifier]')
+        instanceDamage += damage
+    }
+    //console.log(lines)
+    //console.log('                           Total damage for this instance : ' + instanceDamage)
+    console.log(lines)    
     return instanceDamage
- }
-
-function attack(ac, statModifier, proficiencyBonus, advantage, numberOfHits, weaponDamage, damageModifier) {
-    let hitType = hitCalc(ac, statModifier, proficiencyBonus, advantage)
-    if (hitType === "Miss") return 0
-    if (hitType === "Hit") return damageCalc(numberOfHits, weaponDamage, statModifier, damageModifier)
-    if (hitType === "Critical Hit") return (damageCalc(numberOfHits, weaponDamage, statModifier, damageModifier)+damageCalc(numberOfHits, weaponDamage, statModifier, damageModifier))
 }
 
-console.log(attack(ac, statModifier, proficiencyBonus, advantage, numberOfHits, weaponDamage, damageModifier) + ' Damage dealt')
+console.log('                                   ' + attack(ac, statModifier, proficiencyBonus, advantage) + ' Damage dealt')
+console.log(lines)
